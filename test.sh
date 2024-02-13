045d8e62ed2e2ff9d4f801072aaddd8d037278d5
Michael Lodder <redmike7@gmail.com>
2024-02-13 09:39:18 -0700

    update to 0.8.0
    Signed-off-by: Michael Lodder <redmike7@gmail.com>

--- test.sh
+++ test.sh
@@ -1,5 +1,7 @@
-#!/bin/zsh
+#!/bin/bash
 
+echo "Building w/Crypto-BigInt"
+cargo test --no-default-features --features=crypto
 echo "Testing w/Gnu MP Lib"
 cargo test --no-default-features --features=gmp
 echo "Testing w/OpenSSL"
