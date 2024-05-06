9e0f795ff4ae7394593a79504a30332d2b01ad8e
Michael Lodder <redmike7@gmail.com>
2024-05-06 11:23:52 -0600

    updates
    Signed-off-by: Michael Lodder <redmike7@gmail.com>

--- src/gcd_result.rs
+++ src/gcd_result.rs
@@ -2,15 +2,30 @@
     Copyright Michael Lodder. All Rights Reserved.
     SPDX-License-Identifier: Apache-2.0
 */
-use crate::BigNumber;
 
+#[cfg(any(feature = "gmp", feature = "openssl", feature = "rust"))]
 /// GcdResult encapsulates the gcd result and the Bézout coefficients
 #[derive(Debug, Clone)]
 pub struct GcdResult {
     /// Quotient
-    pub gcd: BigNumber,
+    pub gcd: crate::BigNumber,
     /// Bézout coefficient
-    pub x: BigNumber,
+    pub x: crate::BigNumber,
     /// Bézout coefficient
-    pub y: BigNumber,
+    pub y: crate::BigNumber,
+}
+
+#[cfg(feature = "crypto")]
+/// GcdResult encapsulates the gcd result and the Bézout coefficients
+#[derive(Debug, Clone)]
+pub struct GcdResult<const LIMBS: usize>
+where
+    crypto_bigint::Uint<LIMBS>: crypto_bigint::Encoding,
+{
+    /// Quotient
+    pub gcd: crate::SizedBigNumber<LIMBS>,
+    /// Bézout coefficient
+    pub x: crate::SizedBigNumber<LIMBS>,
+    /// Bézout coefficient
+    pub y: crate::SizedBigNumber<LIMBS>,
 }
